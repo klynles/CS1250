bf9f65865969d2b195fb09d136c4163744414522
klynles <31394233+klynles@users.noreply.github.com>
2017-08-27 18:49:03 -0500

    project 1 works

---
+++ Project 1/CS1250-Project1.cpp
@@ -0,0 +1,9 @@
+// A simple C++ program
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Programming is great fun!!"<<endl;
+	cout << "I am a physcis major and this is my first time progamming." <<endl;
+	return 0;
+}
